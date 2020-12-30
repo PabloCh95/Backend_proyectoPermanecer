7ff21a8800b3ef92ad7572443903d7290c9871fd
PabloCh95 <58525405+PabloCh95@users.noreply.github.com>
2020-12-30 09:09:58 -0300

    arreglos en el useContext y Api

--- src/MongoDB/DatabaseConnect.js
+++ src/MongoDB/DatabaseConnect.js
@@ -1,15 +1,15 @@
-const mongoose=require('mongoose');
-const URL="mongodb+srv://admin1:39208224@bdprueba-ulgao.gcp.mongodb.net/Permanecer?retryWrites=true&w=majority";
-module.exports= ()=>{
+const mongoose = require('mongoose');
+const URL = "mongodb://admin123:admin123@basededatos1-shard-00-00.7nwet.mongodb.net:27017,basededatos1-shard-00-01.7nwet.mongodb.net:27017,basededatos1-shard-00-02.7nwet.mongodb.net:27017/basededatos1?ssl=true&replicaSet=atlas-vy84mu-shard-0&authSource=admin&retryWrites=true&w=majority";
+module.exports = () => {
     mongoose.set('useCreateIndex', true);
     //le agregue esta nueva instruccion para solucionar el error
     //DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
-    mongoose.connect(URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(()=>{
+    mongoose.connect(URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
         console.log('DB conectada');
-    }).catch((err)=>{
+    }).catch((err) => {
         console.error(err);
     });
 }
